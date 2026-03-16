6be3ebe2111e5737c285a0b92e6813cd89ad9564
Brian Bauska <brianbauska@gmail.com>
2026-03-15 19:34:48 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 /* 3d-Readme/js/script.js to place text "Todays news..." into constNews. */
-const constNews = `<p>EVERY accusation is a confession. Tax cuts for ONLY the very rich with wealth of 
-over 100 million. Anyone with wealth of over 1 million now pays less taxes than the working man. 
-Time to pay up, assholes. You know who you are. Uncomfortable truth. MAGA are dumb 
-fucks with way too much power. It is jail time. There is nothing to celebrate on America's 250th 
-anniversary thanks entirely to MAGA and the dumb, truly, dumbest humans in the USA. Two words, Fuck & You. 
-Fuck You MAGA. Trump is a very useful idiot for Vladamir Putin. Dumb as a stump but thinks otherwise. Easy 
-to manipulate for Putin because they've been 'friends' since the early 1980's. Long history of Love and 
-Repect if either could do either. Which they can not. Example; Trump paid 41 million for an estate in Florida.
-Then sold it to a Russian oligarch for 95 million. That ain't legal. That ain't moral. Trump has NO morals. 
-None. Of the 341.7 million Americans, how many are being affected by Trump's policies? 341.7 million. All.
+const constNews = `<p>EVERY accusation by Republicans and President Trump is, in fact, a confession. Tax cuts 
+for ONLY the very rich. Anyone with wealth of over 1 million now pays less taxes than the working man. 
+There is nothing to celebrate on America's 250th anniversary thanks entirely to MAGA and the dumb in the USA. 
+Two words, Fuck & You. Fuck You MAGA. Trump is a very useful idiot for Vladamir Putin. Dumb as a stump but he thinks 
+otherwise. Easy to manipulate for Putin because they've been 'friends' since the early 1980's. Long history of Love and 
+Repect if either could do either. An example of Trump's grift; Trump paid 41 million for an estate in Florida. 
+Then sold it to a Russian oligarch for 95 million just a few months late. That ain't legal. 
+Of the 341.7 million Americans, how many are affected by Trump's policies? 341.7 million. All.
 Of the 341.7 million Americans, how many are being NEGATIVELY EFFECTED by Trump's policies? 341.699. In 
 other wordes, everyone but the .0001% are being fucked by this one man &amp; his sycophants. Fuck them 
 ALL. Indict them all. I want justice. I deserve justice for the 341.699 million Americans who made this 
