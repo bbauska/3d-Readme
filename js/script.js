5a12547b35ac6521cf463e1edd329d5553c11975
Brian Bauska <brianbauska@gmail.com>
2026-03-15 19:11:58 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,19 @@
 /* 3d-Readme/js/script.js to place text "Todays news..." into constNews. */
-const constNews = `EVERY accusation is a confession.
-
+const constNews = `<p>EVERY accusation is a confession.
 Jerk off the very rich. over 100 million. pay up assholes. pay up assholes. pay up assholes.
 you know who you are.
-
 Uncomfortable truth. MAGA are dumb fucks with way too much power. Jail time. Or, death.
-
 There is nothing to celebrate on America's 250th anniversary thanks entirely by MAGA and the
 dumb, truly, dumbest humans in the USA. Two words, Fuck & You. Fuck You MAGA.
-
 Trump is a very useful idiot for Vladamir Putin. Dumb as a stump but thinks otherwise.
 Easy to manipulate for Putin because they've been 'friends' since the early 1980's. Long
 history of Love and Repect if either could do either. Which they can not.
-
 Example; paid 41 million, sold next month 95 million. That aint legal. That ain't moral. Trump
 has NO morals. None.
-
 Of the 341.7 million Americans, how many are being affected? 341.7 million.
-
 Of the 341.7 million Americans, how many are being NEGATIVELY EFFECTED by Trump's policies? 341.699.
-
 In other wordes, everyone but the .0001% are being fucked by this one man &amp; his sycophants.
-
 Fuck them ALL.
-
 Indict them all. I want justice. I deserve justice for the 341.699 million Americans who made this nation
 what it is today. Not for those fuck-faces who are bulldozing it.
      -Brian Bauska (bbauska)</p>`;
